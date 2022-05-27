0d17f406d159d98b3356deaa230f8c743963a552
Chino1235 <54426740+Chino1235@users.noreply.github.com>
2022-05-27 10:10:30 +0800

    管理端代码框架编写

---
+++ src/Manager/ClientManager.java
@@ -0,0 +1,23 @@
+package Manager;
+
+public class ClientManager implements Manager{
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void change() {
+
+    }
+
+    @Override
+    public void query() {
+
+    }
+}
