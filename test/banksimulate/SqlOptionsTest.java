b92bf64065d8b5ae06f58c9e150887763aabc2e1
Chino1235 <54426740+Chino1235@users.noreply.github.com>
2022-05-31 19:42:33 +0800

    Create SqlOptionsTest.java
    创建sqloption单元测试

---
+++ test/banksimulate/SqlOptionsTest.java
@@ -0,0 +1,40 @@
+package banksimulate;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SqlOptionsTest {
+
+    @Test
+    public void executeSql() {
+    }
+
+    @Test
+    public void queryProfile() {
+    }
+
+    @Test
+    public void queryPassword() {
+    }
+
+    @Test
+    public void queryName() {
+    }
+
+    @Test
+    public void accountExist() {
+    }
+
+    @Test
+    public void queryClientLog() {
+    }
+
+    @Test
+    public void queryAllLog() {
+    }
+
+    @Test
+    public void queryFinancialProd() {
+    }
+}
