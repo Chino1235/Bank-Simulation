ee1080ffb708fb33b6c77b015a9aabc3eeb38ae0
Chino1235 <54426740+Chino1235@users.noreply.github.com>
2022-05-31 11:53:56 +0800

    Update LogView.java
    修正错误

--- src/Manager/LogView.java
+++ src/Manager/LogView.java
@@ -8,21 +8,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import static banksimulate.SqlOptions.queryAllLog;
+
 public class LogView {
 
-    public static void LogView(String[] args) {
+    public static void logView(String[] args) {
 
-        SqlOptions dbdriver = new SqlOptions();
 
         try {
             //要执行的SQL语句
-            String sql = "select * from bank_log.banklog";
             //3.ResultSet类，用来存放获取的结果集！！
-            ResultSet rs = dbdriver.queryAllLog();
+            ResultSet rs = queryAllLog();
             System.out.println("-----------------");
             System.out.println("执行结果如下所示:");
             System.out.println("-----------------");
-            System.out.println("账户" + "\t" + "配置" + "\t" + "财务" + "\t");
+            System.out.println("账户" + "\t" + "金额" + "\t" + "操作" + "\t");
             System.out.println("-----------------");
 
             String account = null;
