8a235ae63700e1e478398c18ff58fb943ab3596b
Chino1235 <54426740+Chino1235@users.noreply.github.com>
2022-05-31 15:40:57 +0800

    Update TransferForm.java
    转账功能实现

--- src/uiDesigning/TransferForm.java
+++ src/uiDesigning/TransferForm.java
@@ -1,19 +1,24 @@
 package uiDesigning;
 
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.sql.SQLException;
+
+import static banksimulate.SqlOptions.*;
 
 public class TransferForm {
-    Label time = new Label("¶Ô·½ÕËºÅ£º");
+    Label thisacc = new Label("ÎÒ·½ÕËºÅ£º");
+    Label account = new Label("¶Ô·½ÕËºÅ£º");
     Label profile = new Label("½ð¶î£º");
-    TextField timetxt = new TextField();
+    TextField thisactxt = new TextField();
+    TextField acctxt = new TextField();
     TextField profiletxt = new TextField();
     Button confirm = new Button("È·¶¨");
     Button cancel = new Button("È¡Ïû");
 
+    Panel p0 = new Panel();
     Panel p1 = new Panel();
     Panel p2 = new Panel();
     Panel p3 = new Panel();
@@ -21,12 +26,17 @@ public class TransferForm {
     Frame mainFrame = new Frame();
 
     public TransferForm(){
-        mainFrame.setLayout(new GridLayout(3,1));
+        mainFrame.setLayout(new GridLayout(4,1));
+
+        mainFrame.add(p0);
+        p0.setLayout(new BorderLayout());
+        p0.add(thisacc,BorderLayout.WEST);
+        p0.add(thisactxt);
 
         mainFrame.add(p1);
         p1.setLayout(new BorderLayout());
-        p1.add(time,BorderLayout.WEST);
-        p1.add(timetxt);
+        p1.add(account,BorderLayout.WEST);
+        p1.add(acctxt);
 
         mainFrame.add(p2);
         p2.setLayout(new BorderLayout());
@@ -41,7 +51,7 @@ public class TransferForm {
         mainFrame.setVisible(true);
 
 
-        timetxt.setSize(200,50);
+        acctxt.setSize(200,50);
         profiletxt.setSize(200,50);
 
         mainFrame.addWindowListener(new WindowAdapter() {
@@ -53,5 +63,30 @@ public class TransferForm {
         mainFrame.setTitle("×ªÕË");
 
         cancel.addActionListener(e -> mainFrame.setVisible(false));
+
+        confirm.addActionListener(e -> {
+            String thisAccount = thisactxt.getText();
+            String account = acctxt.getText();
+            double profile = Double.parseDouble(profiletxt.getText());
+            try {
+                double profileOfThis = queryProfile(thisAccount);
+                if(profileOfThis<profile){
+                    JOptionPane.showMessageDialog(null,"Óà¶î²»×ã","´íÎó", JOptionPane.ERROR_MESSAGE);
+                } else if(accountExist(thisAccount) || accountExist(account)) {
+                    JOptionPane.showMessageDialog(null,"ÕËºÅÊäÈë´íÎó","´íÎó", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    double accProfile = queryProfile(account);
+                    double accProfileNow = accProfile + profile;
+                    double accThisProfile = queryProfile(thisAccount);
+                    double accThisProfileNow = accThisProfile - profile;
+                    executeSql("UPDATE bank.client SET profile='"+accProfileNow+"' WHERE account='"+account+"'; ");
+                    executeSql("UPDATE bank.client SET profile='"+accThisProfileNow+"' WHERE account='"+thisAccount+"'; ");
+                }
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null,"ÏµÍ³´íÎó","´íÎó",JOptionPane.ERROR_MESSAGE);
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"SQL´íÎó","´íÎó",JOptionPane.ERROR_MESSAGE);
+            }
+        });
     }
 }
