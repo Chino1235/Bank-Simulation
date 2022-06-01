6f9aedf818efd61f023eb10932543e838ccb4db2
LIEBE1024 <3202375751@qq.com>
2022-06-01 09:33:51 +0800

    Create ClientManagerTest.java
    创建客户管理单元测试
    
    Co-Authored-By: Chino1235 <54426740+Chino1235@users.noreply.github.com>

---
+++ test/Manager/ClientManagerTest.java
@@ -0,0 +1,28 @@
+package Manager;
+
+import junit.framework.TestCase;
+
+public class ClientManagerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testAdd() {
+    }
+
+    public void testDelete() {
+    }
+
+    public void testChange() {
+    }
+
+    public void testQuery() {
+    }
+
+    public void testMenu() {
+    }
+}
