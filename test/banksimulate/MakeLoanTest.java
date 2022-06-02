2892588085e7f53c4c58c2ee94ae04307a3719d2
10ovo01 <104555438+10ovo01@users.noreply.github.com>
2022-06-02 10:56:10 +0800

    FinancialProductManager.java

--- test/banksimulate/MakeLoanTest.java
+++ test/banksimulate/MakeLoanTest.java
@@ -7,11 +7,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MakeLoanTest {
-
+    MakeLoan makeLoan = new MakeLoan();
     @Before
     public void setUp() throws Exception {
-       loan();
-
+        new MakeLoan();
     }
 
     @After
@@ -22,10 +21,24 @@ public class MakeLoanTest {
     @Test
     public void loan() {
         double invest = 10000;
+        double yearRate = 0.0525;
+        double monthRate = yearRate/12;
+        int month = 12;
+        double monthIncome = (invest* monthRate * Math.pow(1+monthRate,month))/(Math.pow(1+monthRate,month)-1);
+        String result = String.format("%.2f", monthIncome);
+        assertEquals(1,1);
+
+
     }
 
     @Test
-    public void getMonthIncome() {
+    public void getMonthIncome(){
+        double invest = 10000;
+        double yearRate = 0.0525;
+        double monthRate = yearRate/12;
+        int month = 12;
+        float result = (float) makeLoan.getMonthIncome(10000, 0.525, 12);
+        assertEquals(1088.8342,result,0.0001);
 
     }
 }
